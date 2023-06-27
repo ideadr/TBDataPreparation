2318d87b9bc72138c28ef64cef63623e993c3b8c
ideadr <ideadr@cern.ch>
2023-06-27 12:19:58 +0200

    Improved versatility of script

--- 2023_SPS/SIPM/scripts/convert.py
+++ 2023_SPS/SIPM/scripts/convert.py
@@ -4,11 +4,12 @@ import multiprocessing as mp
 from tqdm import tqdm
 
 
-rawdataPath = "/afs/cern.ch/user/i/ideadr/cernbox/TB2021_H8/rawData"
-rawntuplePath = "/afs/cern.ch/user/i/ideadr/cernbox/TB2021_H8/rawNtupleSiPM"
+#rawdataPath = "/afs/cern.ch/user/i/ideadr/cernbox/TB2021_H8/rawData"
+#rawntuplePath = "/afs/cern.ch/user/i/ideadr/cernbox/TB2021_H8/rawNtupleSiPM"
 
 
 def getFiles():
+    print(rawdataPath)
     files = glob(rawdataPath + "/*.dat")
     files = list(map(os.path.abspath, files))
     files = [f for f in files if os.path.getsize(f)/1024/1024 > 3]
@@ -18,7 +19,7 @@ def getFiles():
 def moveConverted(fnames):
     toMove = glob(rawdataPath + "/Run*.root")
     for f in toMove:
-        newfname = f.replace("rawData","rawNtupleSiPM")
+        newfname = rawntuplePath +"/"+ os.path.basename(f)
         os.rename(f,newfname)
     for f in fnames:
         p = subprocess.Popen(["bzip2", "-z", f])
@@ -46,7 +47,28 @@ def convertAll(fnames):
 
 
 if __name__ == "__main__":
+    import argparse                                                                      
+    parser = argparse.ArgumentParser(description='This script onverts the binary FERs files from the Janus software to root ntuples.')
+    parser.add_argument('-i', '--inputRawDataPath', dest='rawdataPath', default='/afs/cern.ch/user/i/ideadr/cernbox/TB2023_H8/rawData', help='Input path of raw data')
+    parser.add_argument('-o', '--outputRawNtuplePath', dest='rawntuplePath', default='/afs/cern.ch/user/i/ideadr/cernbox/TB2023_H8/rawNtupleSiPM', help='Output path for the raw ntuples')
+    
+    par  = parser.parse_args()
+    global rawdataPath 
+    rawdataPath = par.rawdataPath
+    global rawntuplePath
+    rawntuplePath = par.rawntuplePath
+
+    if not (os.path.isdir(rawdataPath)):
+        print(f"ERROR: {rawdataPath} does not exist")
+        exit()
+
+    if not (os.path.isdir(rawntuplePath)):
+        print(f"ERROR: {rawntuplePath} does not exist")
+        exit()
+    
     toConvert = getFiles()
+
+    print(toConvert)
 
     convertAll(toConvert)
     moveConverted(toConvert)
